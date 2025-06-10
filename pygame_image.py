7beb922e3ae85b2cf9a13ed06597e3312e5b7ca8
Tatsuki <c0a24036b3@edu.teu.ac.jp>
2025-06-10 16:12:03 +0900

    練習10:押下キーに応じたこうかとんの移動

--- pygame_image.py
+++ pygame_image.py
@@ -13,16 +13,28 @@ def main():
     bg_img_f = pg.transform.flip(bg_img,True,False) #背景画像のSurface
     kk_img = pg.image.load("fig/3.png") #こうかとん画像のSurface
     kk_img = pg.transform.flip(kk_img,True,False) #こうかとん画像の左右反転
+    kk_rct = kk_img.get_rect() #こうかとんSurfaceに対応するこうかとんRectの取得
+    kk_rct.center = 300, 200
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]: #上移動
+            kk_rct.move_ip((0,-1))
+        if key_lst[pg.K_DOWN]: #下移動
+            kk_rct.move_ip((0,1))
+        if key_lst[pg.K_LEFT]: #左移動
+            kk_rct.move_ip((-1,0))
+        if key_lst[pg.K_RIGHT]: #右移動
+            kk_rct.move_ip((1,0))
 
         x=tmr%3200 #3200フレーム後にもとに戻る（ループ）
         screen.blit(bg_img, [-x,0]) #背景画像のblit
         screen.blit(bg_img_f, [-x+1600,0])#背景画像を1600フレーム後にblit
         screen.blit(bg_img, [-x+3200,0]) #背景画像を3200フレーム後にblit
-        screen.blit(kk_img, [300, 200]) #こうかとん画像のblit
+        #screen.blit(kk_img, [300, 200]) #こうかとん画像のblit
+        screen.blit(kk_img,kk_rct)#こうかとん画像のblit
         pg.display.update()
         tmr += 1        
         clock.tick(200)
